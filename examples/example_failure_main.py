6f26be3855c36f7c11abaa8c8fd5d4df256dabd7
MBijman <bijman.m.m@gmail.com>
2018-06-20 10:02:38 +0000

    Changed failure example to use full features of the callback failure function

--- examples/example_failure_main.py
+++ examples/example_failure_main.py
@@ -9,22 +9,31 @@ config = {
     "maxsize": 100
 }
 
+###
+# Handle failed func
+
+
 def produce(items):
     r = RedisQueue(**config)
     for i in range(items):
         r.send(i)
 
+
 def my_func(item, worker_id):
-    print('got item')
+    print('got item {}'.format(item))
     time.sleep(1)
-    if item > 5:
+    if int(item) > 5:
         raise ValueError
     print('finished item', locals())
 
+
 def my_failure_func(item, exception, r_instance, worker_id):
-    print(item, str(exception), worker_id)
+    print('failure callback', item, str(exception), worker_id)
+    int_item = int(item.decode('utf-8'))
+    # handle item, and resend to queue with item minus one
+    r_instance.send(int_item-1)
 
 if __name__ == "__main__":
     produce(10)
-    startapp(myfunc, workers=10, config=config, on_failure_func=my_failure_func)
+    startapp(my_func, workers=10, config=config, on_failure_func=my_failure_func)
 
