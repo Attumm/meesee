5e6ae83d97101d37f94bfcc73405b5a8a2c56528
Attumm <mbijman.m.m@gmail.com>
2018-07-13 04:16:17 +0200

    Added initialization within own process for workers

---
+++ examples/example_init_kwargs.py
@@ -0,0 +1,59 @@
+import time
+import random
+
+from meesee import RedisQueue
+from meesee import startapp
+
+
+config = {
+    "namespace": "removeme",
+    "key": "tasks",
+    "redis_config": {},
+    "maxsize": 10000,
+    "timeout": 1,
+}
+
+
+class SchroderCat:
+    def __init__(self, name):
+        self.name = name
+        self.alive = None
+
+    def __str__(self):
+        status = str(self.alive) if self.alive is not None else 'Unknown'
+        return 'name: {} is_alive: {}'.format(self.name, status)
+
+    def __repr__(self):
+        return str(self)
+
+    def look_in_the_box(self):
+        if self.alive is None:
+            self.alive = bool(random.getrandbits(1))
+        return self.alive
+
+
+def produce(items):
+    r = RedisQueue(**config)
+    for i in range(items):
+        r.send(i)
+
+
+def myfunc(item, worker_id, ana, bob, citrus):
+    time.sleep(1)
+    ana.look_in_the_box()
+    bob.look_in_the_box()
+    citrus.look_in_the_box()
+    print('finished item', locals())
+
+
+if __name__ == "__main__":
+    # produce 100 items
+    produce(100)
+
+    names = ['ana', 'bob', 'citrus']
+    kwargs = {name: SchroderCat for name in names}
+    init_kwargs = {name: {'name': name} for name in names}
+
+
+    # stop with keyboard intrupped
+    startapp(myfunc, workers=10, config=config, func_kwargs=kwargs, init_kwargs=init_kwargs)
--- meesee.py
+++ meesee.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import json
+import traceback
 import redis
 
 from multiprocessing import Pool
@@ -52,14 +53,32 @@ class RedisQueue:
         return result
 
 
-def run_worker(func, func_kwargs, on_failure_func, config, worker_id):
+class InitFail(Exception):
+    pass
+
+
+def init_add(kwargs, init_kwargs):
+    try:
+        for name, config in init_kwargs.items():
+            kwargs[name] = kwargs[name](**config)
+    except TypeError as e:
+        raise InitFail from e
+    return kwargs
+
+
+def run_worker(func, func_kwargs, on_failure_func, config, worker_id, init_kwargs):
     item, r = None, None
     while True:
         try:
+            func_kwargs = init_add(func_kwargs, init_kwargs)
             r = RedisQueue(**config) # TODO rename r
             sys.stdout.write('worker {worker_id} started\n'.format(worker_id=worker_id))
             for key_name, item in r:
                 func(item.decode('utf-8'), worker_id, **func_kwargs)
+        except InitFail:
+            sys.stdout.write('worker {worker_id} initialization failed\n'.format(worker_id=worker_id))
+            traceback.print_exc()
+            break
         except (KeyboardInterrupt, SystemExit):
             sys.stdout.write('worker {worker_id} stopped\n'.format(worker_id=worker_id))
             r.first_inline_send(item)
@@ -75,9 +94,10 @@ def run_worker(func, func_kwargs, on_failure_func, config, worker_id):
             break
 
 
-def startapp(func, func_kwargs={}, workers=10, config=config, on_failure_func=None):
+def startapp(func, func_kwargs={}, workers=10, config=config, on_failure_func=None, init_kwargs={}):
     p = Pool(workers)
-    args = ((func, func_kwargs, on_failure_func, config, worker_id) for worker_id in range(1, workers+1))
+    args = ((func, func_kwargs, on_failure_func, config, worker_id, init_kwargs) for worker_id in
+            range(1, workers + 1))
     try:
         p.starmap(run_worker, args)
     except (KeyboardInterrupt, SystemExit):
