b5a631d417d25e6ec6e579311c2d73c4656a2b83
Attumm <bijman.m.m@gmail.com>
2021-02-11 16:25:05 +0100

    Added description for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import io
+
 from setuptools import setup
+
+current = path.abspath(path.dirname(__file__))
+
+with io.open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='meesee',
     author='Melvin Bijman',
     author_email='bijman.m.m@gmail.com',
-    version='0.9.2',
+
+    description='Task queue, Long lived workers process parallelization, with Redis as backend',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    version='0.9.4',
     py_modules=['meesee'],
     install_requires=['redis==2.10.6'],
     license='MIT',
