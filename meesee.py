a6c18dd47911ab89651d89a605ba9ba5bf96b37b
Attumm <bijman.m.m@gmail.com>
2024-03-05 14:13:25 +0100

    Changed pylama

--- meesee.py
+++ meesee.py
@@ -36,10 +36,10 @@ class RedisQueue:
     def first_inline_send(self, item):
         # TODO rename method
         self.r.lpush(self.list_key, item)
-    
+
     def send_to(self, key, item):
         self.r.rpush('{}:{}'.format(self.namespace, key), item)
-        
+
     def send(self, item):
         """Adds item to the end of the Redis List.
 
@@ -78,7 +78,7 @@ class RedisQueue:
         if result is None:
             raise StopIteration
         return result
-    
+
     def __len__(self):
         return self.r.llen(self.list_key)
 
@@ -102,9 +102,9 @@ def setup_init_items(func_kwargs, init_kwargs):
 
 def run_worker(func, func_kwargs, on_failure_func, config, worker_id, init_kwargs):
     if isinstance(func, list):
-        func = func[worker_id%len(func)]
+        func = func[worker_id % len(func)]
     if isinstance(config, list):
-        config = config[worker_id%len(config)]
+        config = config[worker_id % len(config)]
 
     item, r = None, None
     init_items = setup_init_items(func_kwargs, init_kwargs)
@@ -138,8 +138,8 @@ def run_worker(func, func_kwargs, on_failure_func, config, worker_id, init_kwarg
 
 def startapp(func, func_kwargs={}, workers=10, config=config, on_failure_func=None, init_kwargs={}):
     with Pool(workers) as p:
-        args = ((func, func_kwargs, on_failure_func, config, worker_id, init_kwargs) for worker_id in
-                range(1, workers + 1))
+        args = ((func, func_kwargs, on_failure_func, config, worker_id, init_kwargs)
+                for worker_id in range(1, workers + 1))
         try:
             p.starmap(run_worker, args)
         except (KeyboardInterrupt, SystemExit):
